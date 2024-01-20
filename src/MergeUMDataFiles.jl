d3eb5108fbbf6f8216041a37530e30efa553ca56
Krishna Bhogaonker <cyclotomiq@gmail.com>
2024-01-20 10:19:19 -0500

    working on getting the function to extract unique professor and department combinations created.

--- src/MergeUMDataFiles.jl
+++ src/MergeUMDataFiles.jl
@@ -1,7 +1,7 @@
 
 
 
-function merge_new_um_data_into_single_file(filepath::String, newfilename::String)
+function merge_new_um_data_into_single_file(filepath::String, newfilename::String; to_csv::Bool=true)
 
     # Start the logging stuff
     logger = TeeLogger(
@@ -39,7 +39,7 @@ function merge_new_um_data_into_single_file(filepath::String, newfilename::Strin
     # for each file in list, read the data into a dataframe.
         @debug("file opened: $(joinpath(filepath, f))")
         tdf = DataFrame(CSV.File(joinpath(filepath, f)))
-        rename!(tdf, [:campus, :name, :jobcode, :orgname, :ftr, :basis, :fraction, :gf])
+        rename!(tdf, [:campus, :name, :jobdesc, :orgname, :ftr, :basis, :fraction, :gf])
 
         # append a column to the dataframe for the year, university 
         reg = r"[0-9]+"
@@ -51,16 +51,71 @@ function merge_new_um_data_into_single_file(filepath::String, newfilename::Strin
             combined_df = tdf
             @debug("Created new combined dataframe.")
         else
-            #@debug("head: $(first(tdf, 5))")
             append!(combined_df, tdf, promote=true)
             @debug("appended file to combined dataframe.")
         end
     end
 
+    for c in [:campus, :name, :jobdesc, :orgname]
+        combined_df = transform(combined_df, c => ByRow(passmissing(lowercase)) => c)
+    end
     
-    CSV.write(joinpath(filepath, newfilename), combined_df)
-    @debug("wrote file to directory.")
-    @debug("closing log.")
+
+    @debug("replace missing values in orgname and jobdesc.")
+
+    if to_csv    
+        CSV.write(newfilename, combined_df)
+        @debug("wrote files to directory.")
+        @debug("closing log.")
+    end
+
+    return combined_df
+end
 
 
+function _extract_unique_department_prof_names_from_new_um_data(fname::String; to_csv=true)
+    logger = TeeLogger(
+            ConsoleLogger(stderr),
+            FormatLogger(open("logfile_unique_names.txt", "w")) do io, args
+            # Write the module, level and message only
+                println(io, args._module, " | ", "[", args.level, "] ", args.message)
+            end )
+    
+    global_logger(logger)
+    # First sort the dataframe by name. I am really trying to remove the same names.
+
+    tdf = DataFrame(CSV.File(fname))
+
+    @debug("loaded the file to dataframe")
+    
+    tdf = sort(tdf, [:name])
+
+    @debug("sorted file by name")
+
+    # Then I want to get the unique name and department combos. This will remove
+    # the same names from year to year.    
+    tdf = unique(tdf, [:name, :orgname])
+    @debug("produced unique name and department combos: \n $(first(tdf, 5))")
+    # Next I have to extract only professors. I don't want to look at names that  
+    # are not professors. I will probably need to tune this. I can search for 
+    # just professors and such. 
+
+    valid_jobs_list = ["professor", "assoc professor", "asst professor"]
+
+    tdf = transform(tdf, :jobdesc => ByRow(passmissing(lowercase)) => :jobdesc)
+    @debug("lowercased all of the job descriptions.")
+    
+    replace!(tdf.jobdesc, missing => "")
+    @debug("produced unique name and department combos")
+
+    tdf = filter(:jobdesc => n -> any(occursin.(valid_jobs_list, n)), tdf)
+    @debug("filtered the data \n $(first(tdf, 5))")
+
+    if to_csv    
+        CSV.write("unique_name_depts.csv", tdf)
+        @debug("wrote file to directory.")
+        @debug("closing log.")
+    end
+
+    return tdf
 end
